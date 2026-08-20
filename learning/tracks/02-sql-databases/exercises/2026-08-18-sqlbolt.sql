4e0084a90a5682ce9b7af49b6c03edd9158120ab
Ireland Payne <168472558+IrelandPayne@users.noreply.github.com>
2026-08-19 22:28:02 -0400

    19th updates

--- learning/tracks/02-sql-databases/exercises/2026-08-18-sqlbolt.sql
+++ learning/tracks/02-sql-databases/exercises/2026-08-18-sqlbolt.sql
@@ -235,3 +235,49 @@ WHERE year % 2 = 0;
 -- Exercise 10: -- 
 
 -- Find the longest time that an employee has been at the studio
+SELECT 
+    name, 
+    MAX (years_employed)
+FROM employees;
+
+-- For each role, find the average number of years employed by employees in that role
+SELECT 
+    role,
+    AVG(years_employed) AS AVG_years_employed
+FROM employees
+GROUP BY role;
+
+-- Find the total number of employee years worked in each building
+SELECT 
+    building,
+    SUM (years_employed) AS total_years_employed
+FROM employees
+GROUP BY building;
+
+-- Exercise 11: --
+
+-- Find the number of Artists in the studio (without a HAVING clause)
+SELECT
+    role,
+    COUNT (role) AS Number_of_artists
+FROM employees
+WHERE role = "Artist";
+
+-- Find the number of Employees of each role in the studio
+SELECT 
+    role, 
+    COUNT (name)
+FROM employees
+GROUP BY role;
+
+-- Find the total number of years employed by all Engineers
+SELECT 
+    role, 
+    SUM (years_employed)
+FROM employees
+GROUP BY role
+HAVING role = "Engineer";
+
+-- Exercise 12: -- 
+
+-- 
