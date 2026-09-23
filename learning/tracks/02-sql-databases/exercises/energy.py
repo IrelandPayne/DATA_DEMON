8d20a665d12b049d215d5e29942fd2667cac3500
Ireland Payne <168472558+IrelandPayne@users.noreply.github.com>
2026-09-23 19:50:20 -0400

    homework

--- learning/tracks/02-sql-databases/exercises/energy.py
+++ learning/tracks/02-sql-databases/exercises/energy.py
@@ -6,8 +6,80 @@ import sqlite3
 import sys
 
 
-# replace this comment with your implementation of the EnergyDB class
+class EnergyDB():
+    """ An in-memory SQLite database that stores electricity production records.
 
+    Attributes:
+        conn (sqlite3.Connection): An in-memory SQLite database connection
+            to query energy production data.
+    """
+    def __init__(self, filename):
+        """ Initializes the database connection and loads data from a file.
+
+        Args:
+            filename (str): Path to the CSV file containing energy production
+                data.
+
+        Side effects:
+            Initializes an SQLite connection and populates the database
+            through invoking the read() method.
+        """
+        self.conn = sqlite3.connect(':memory:')
+        self.read(filename)
+       
+    def __del__(self):
+        """ Clean up the database connection. """
+        try:
+            self.conn.close()
+        except:
+            pass
+        
+    def read(self, filename):
+        """ Read energy data from a CSV file and insert it into the SQLite table.
+
+        Args:
+            filename (str): Path to the CSV file to read.
+
+        Side effects:
+            Creates the 'production' table, inserts rows from
+            the file, and commits the changes to self.conn.
+        """
+        cur = self.conn.cursor()
+        cur.execute('CREATE TABLE production (year integer, state text, source text, mwh real)')
+        
+        with open (filename, 'r', encoding = 'utf-8') as file:
+            file.readline()
+            
+            for line in file: 
+                cleaned = line.strip().split(',')
+                Year = int(cleaned[0])
+                State = cleaned[1]
+                Energy_Source = cleaned[2]
+                Megawatthours = float(cleaned[3])
+                
+                cur.execute('INSERT INTO production VALUES (?,?,?,?)',
+                            (Year, State, Energy_Source, Megawatthours))
+        
+        self.conn.commit()
+                
+    def production_by_source(self, source, year):
+        """ Calculate total electricity production for a given source and year.
+
+        Args:
+            source (str): An energy source name.
+            year (int): The production year.
+
+        Returns:
+            float: Total megawatt-hours produced across all states for the
+            specified source and year.
+        """
+        cursor = self.conn.cursor()   
+        cursor.execute('SELECT mwh FROM production WHERE source=? AND year=?',
+                        (source, year))
+        
+        all_rows = cursor.fetchall()
+    
+        return sum(row[0] for row in all_rows)   
 
 def main(filename):
     """ Build a database of energy sources and calculate the total production
